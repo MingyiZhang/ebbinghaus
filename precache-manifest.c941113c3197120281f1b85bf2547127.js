0311ef0e9a9475423d0f8b7865785571188372e1
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-12 22:33:57 +0200

    Updates

--- precache-manifest.c941113c3197120281f1b85bf2547127.js
+++ precache-manifest.c941113c3197120281f1b85bf2547127.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "634f8ce6bb693d35f8965dfe538c4093",
+    "revision": "c5883ce4414133c8133c674f0ef71c9d",
     "url": "/EbbyCode/index.html"
   },
   {
-    "revision": "adb9a15ec490e15f1dc3",
+    "revision": "510415af968627b539ad",
     "url": "/EbbyCode/static/css/main.f53ffc58.chunk.css"
   },
   {
-    "revision": "0ebf8b1982404b34e988",
-    "url": "/EbbyCode/static/js/2.3cef5206.chunk.js"
+    "revision": "32c1f7ae6b77ee78d07b",
+    "url": "/EbbyCode/static/js/2.262c4dfb.chunk.js"
   },
   {
     "revision": "eac4cf64bf573541fd6dba29c78393a0",
-    "url": "/EbbyCode/static/js/2.3cef5206.chunk.js.LICENSE.txt"
+    "url": "/EbbyCode/static/js/2.262c4dfb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "adb9a15ec490e15f1dc3",
-    "url": "/EbbyCode/static/js/main.0955a232.chunk.js"
+    "revision": "510415af968627b539ad",
+    "url": "/EbbyCode/static/js/main.54c53b6f.chunk.js"
   },
   {
     "revision": "a20e348757d295932250",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EbbyCode/precache-manifest.41055e4eef918f05f3b3dac917cce25f.js"
+  "/EbbyCode/precache-manifest.c941113c3197120281f1b85bf2547127.js"
 );
 
 self.addEventListener('message', (event) => {
